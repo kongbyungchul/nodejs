9772a733d9a5858c0295fae49c555ee43fc0324a
kong byungchul <qudcjfchlrh@naver/com>
2020-05-21 17:13:28 +0900

    ejs 등록

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express')
-
+var path = require('path')
+var bodyParser =require('body-parser')
 var app = express()
+
+app.set('views',path.join(__dirname+'/views'))
+app.set('view engine', 'ejs')
+
+//app.use()
+
 app.get('/',function(request,response){
-console.log(request)
-response.send("<h1 style='color:red'>안녕하세요</h1>")
+//console.log(request)
+response.render('index')
 })
 //listen(포트, url, backlog, callback)
 app.listen(8080,function(){
