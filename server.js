4f86d4a1b0f9282d0f18e329e8076e2113a11f1b
kong byungchul <qudcjfchlrh@naver/com>
2020-05-21 16:43:13 +0900

    nodejs practice

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+
+var app = express()
+app.get('/',function(request,response){
+console.log(request)
+response.send("<h1 style='color:red'>안녕하세요</h1>")
+})
+//listen(포트, url, backlog, callback)
+app.listen(8080,function(){
+    console.log("server is starting at http://localgost:8080")
+})
